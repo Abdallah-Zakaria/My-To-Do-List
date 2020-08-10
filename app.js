c641548302a31bdca9d494ddd8a3f8f42ddf67bf
Your Name In Single QuotesAbdallah-Zakariagit config --global core.editor code <abdallah.zakaria1996@icloud.com>
2020-08-10 11:16:17 +0300

    add1

---
+++ app.js
@@ -0,0 +1,101 @@
+var taskForm = document.getElementById("taskForm")
+
+if (Object.keys(localStorage).length !== 0) {
+    console.log("its not")
+    for (var i = 0; i < Object.keys(localStorage).length; i++) {
+        var taskListAll = JSON.parse(localStorage.getItem("taskListAll"));
+        console.log(taskListAll)
+        for (var i = 0; i < taskListAll.length; i++) {
+
+            var resultTable = document.getElementById("resultTable")
+            var tr = document.createElement("tr")
+            tr.setAttribute("id", taskListAll[i][1])
+            resultTable.appendChild(tr)
+    
+            var td = document.createElement("td")
+            td.textContent = taskListAll[i][1]
+            tr.appendChild(td)
+            var td = document.createElement("td")
+            td.textContent = taskListAll[i][2]
+            tr.appendChild(td)
+            var td = document.createElement("td")
+            td.textContent = taskListAll[i][3]
+            tr.appendChild(td)
+            var delButton = document.createElement("button")
+            delButton.textContent = "X"
+            delButton.setAttribute("id", taskListAll[i][1])
+            delButton.setAttribute("onClick", "reply_click(this.id)")
+            tr.appendChild(delButton)
+        }
+    }
+} else {
+    var taskListAll = []
+}
+
+
+taskForm.addEventListener('submit', function () {
+    event.preventDefault()
+    var task = event.target.task.value
+    var date = event.target.date.value
+    var urgency = event.target.urgency.value // .split(",");
+
+
+
+    var taskList = [task, date, urgency]
+    taskListAll.push(taskList)
+
+
+    var taskListStringify = JSON.stringify(taskListAll)
+    localStorage.setItem("taskListAll", taskListStringify);
+
+
+
+    var resultTable = document.getElementById("resultTable")
+    var tr = document.createElement("tr")
+    tr.setAttribute("id", task)
+    resultTable.appendChild(tr)
+
+    var td = document.createElement("td")
+    td.textContent = task
+    tr.appendChild(td)
+    var td = document.createElement("td")
+    td.textContent = date
+    tr.appendChild(td)
+    var td = document.createElement("td")
+    td.textContent = urgency
+    tr.appendChild(td)
+    var delButton = document.createElement("button")
+    delButton.textContent = "X"
+    delButton.setAttribute("id", task)
+    delButton.setAttribute("onClick", "reply_click(this.id)")
+    tr.appendChild(delButton)
+})
+
+var reply_click = function (clicked_id) {
+    var deletedTask = document.getElementById(clicked_id)
+    deletedTask.remove()
+}
+
+
+// for (var i = 0; i < taskListAll.length; i++) {
+
+//     var resultTable = document.getElementById("resultTable")
+//     var tr = document.createElement("tr")
+//     tr.setAttribute("id", taskListAll[i][1])
+//     resultTable.appendChild(tr)
+
+//     var td = document.createElement("td")
+//     td.textContent = taskListAll[i][1]
+//     tr.appendChild(td)
+//     var td = document.createElement("td")
+//     td.textContent = taskListAll[i][2]
+//     tr.appendChild(td)
+//     var td = document.createElement("td")
+//     td.textContent = taskListAll[i][3]
+//     tr.appendChild(td)
+//     var delButton = document.createElement("button")
+//     delButton.textContent = "X"
+//     delButton.setAttribute("id", taskListAll[i][1])
+//     delButton.setAttribute("onClick", "reply_click(this.id)")
+//     tr.appendChild(delButton)
+// }
